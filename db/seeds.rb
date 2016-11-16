d2c5d32b9849161aec91c701f10bfcb4dbb85106
DonkeyFish456 <stancalderon@gmail.com>
2016-11-16 14:46:24 -0800

    add username to devise and ability to login with username or email.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+        username: 'stan',
+        email:  'stancalderon@gmail.com',
+        password: 'password'
+)
+
+puts 'Added 1 user'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new( username:'Stan', email:'stancalderon@gmail.com', password: 'password', password_confirmation: 'password') }
+
+  describe 'attributes' do
+    it 'should have username and email attributes' do
+      expect(user).to respond_to(:username, :email)
+    end
+  end
+
+  describe 'invalid user' do
+    let(:user_with_invalid_username) { User.new(name: 'Stan', email: 'stan@gmail.com', password: 'password', password_confirmation: 'password') }
+    let(:user_with_invalid_email) { User.new(name: 'StanCalderon', email: 'stancalderon@gmail.com', password: 'password', password_confirmation: 'password')  }
+
+    it 'should be an invalid user due to a duplicate username' do
+      expect(user_with_invalid_username).to_not be_valid
+    end
+  end
 end
