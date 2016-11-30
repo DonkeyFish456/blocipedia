5f863b78a845eff1c82f4a1f0bc8dfd0782c877d
DonkeyFish456 <stancalderon@gmail.com>
2016-11-30 10:24:29 -0800

    refactor subscription controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -10,79 +10,13 @@ class SubscribersController < ApplicationController
   end
 
   def update
-    token = params[:stripeToken]
-    plan_id = params[:plan_id]
-    plan = Stripe::Plan.retrieve(plan_id)
-    role = params[:role]
-    proration_date = Time.now.to_i
-
-
-    if current_user.subscribed
-      customer = Stripe::Customer.retrieve(current_user.stripeid)
-      sub_id = customer['subscriptions']['data'][0]['id']
-
-      subscription = Stripe::Subscription.retrieve(current_user.subscription_id)
-      subscription.plan = plan_id
-      subscription.prorate = true
-      subscription.proration_date = proration_date
-      subscription.save
-    else
-      customer = Stripe::Customer.create(
-          card: token,
-          plan: plan,
-          email: current_user.email
-      )
-
-      sub = Stripe::Customer.retrieve(customer.id)
-      sub_id = sub['subscriptions']['data'][0]['id']
-    end
-
-    current_user.subscribed = true
-    current_user.stripeid = customer.id
-    current_user.planid = plan_id
-    current_user.role = role
-    current_user.subscription_id = sub_id
-    current_user.save
-
-    redirect_to user_path(current_user.id), :notice => "You are now a #{role} member!"
-
+    switch_plan
+    redirect_to user_path(current_user.id), :notice => "You are now a #{params[:role]} member!"
   end
 
   def destroy
-    token = params[:stripeToken]
-    plan_id = '1001'
-    plan = Stripe::Plan.retrieve(plan_id)
-    role = 'member'
-    proration_date = Time.now.to_i
-
-    if current_user.subscribed
-      customer = Stripe::Customer.retrieve(current_user.stripeid)
-      sub_id = customer['subscriptions']['data'][0]['id']
-
-      subscription = Stripe::Subscription.retrieve(current_user.subscription_id)
-      subscription.plan = plan_id
-      subscription.prorate = true
-      subscription.proration_date = proration_date
-      subscription.save
-    else
-      customer = Stripe::Customer.create(
-          card: token,
-          plan: plan,
-          email: current_user.email
-      )
-
-      sub = Stripe::Customer.retrieve(customer.id)
-      sub_id = sub['subscriptions']['data'][0]['id']
-    end
-
+    switch_plan('1001', 'member')
     make_wikis_public(current_user.wiki)
-    current_user.subscribed = true
-    current_user.stripeid = customer.id
-    current_user.planid = plan_id
-    current_user.role = role
-    current_user.subscription_id = sub_id
-    current_user.save
-
     redirect_to user_path(current_user.id), :notice => "You are now a basic member!"
   end
 
@@ -93,5 +27,39 @@ class SubscribersController < ApplicationController
       i['private'] = false
       i.save
     end
+  end
+
+  def switch_plan(plan_id=params[:plan_id], role=params[:role])
+    token = params[:stripeToken]
+    plan_id = plan_id
+    plan = Stripe::Plan.retrieve(plan_id)
+    role = role
+    proration_date = Time.now.to_i
+
+    if current_user.subscribed
+      customer = Stripe::Customer.retrieve(current_user.stripeid)
+      sub_id = customer['subscriptions']['data'][0]['id']
+
+      subscription = Stripe::Subscription.retrieve(current_user.subscription_id)
+      subscription.plan = plan_id
+      subscription.prorate = true
+      subscription.proration_date = proration_date
+      subscription.save
+    else
+      customer = Stripe::Customer.create(
+          card: token,
+          plan: plan,
+          email: current_user.email
+      )
+      sub = Stripe::Customer.retrieve(customer.id)
+      sub_id = sub['subscriptions']['data'][0]['id']
+    end
+
+    current_user.subscribed = true
+    current_user.stripeid = customer.id
+    current_user.planid = plan_id
+    current_user.role = role
+    current_user.subscription_id = sub_id
+    current_user.save
   end
 end
